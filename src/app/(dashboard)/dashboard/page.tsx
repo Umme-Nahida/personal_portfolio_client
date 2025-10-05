815e1500ccb24a53b4e6251271d4b98c6a84200c
Developer Nahida <ummenahida20@gmail.com>
2025-10-05 16:28:14 +0600

    create public layout page

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,25 @@
+
+import UserProfileDashboard from "@/components/modules/dashboard/UserDashoboard";
+import { getUserSession } from "@/utils/getUserSession";
+
+import { getServerSession } from "next-auth";
+
+const DashboardHomePage =async () => {
+
+ const session = await getUserSession()
+   console.log("session",session)
+  
+  return (
+    <div>
+      <h1>Dashboard Home Page</h1>
+      <p>{session?.user?.name} </p>
+      <p>{session?.user?.email}</p>
+
+      <div className="flex items-center justify-center">
+        <UserProfileDashboard/>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardHomePage;
