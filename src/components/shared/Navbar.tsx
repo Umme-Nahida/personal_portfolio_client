2d70b288d1319e0f1f1db0baa3f7a2eab7c9513e
Developer Nahida <ummenahida20@gmail.com>
2025-10-05 16:29:21 +0600

    added logout button in nabvar

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -3,13 +3,15 @@
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
 import Link from "next/link";
+import { signOut, useSession } from "next-auth/react";
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
+    const session = useSession()
 
     return (
         <nav className=" fixed top-0 w-full backdrop-blur-xl bg-sky-400/10 border-y border-y-white/20  shadow-lg py-2 z-50 ">
-           
+
             <div className="container mx-auto px-22">
                 <div className="flex justify-between items-center px-6 py-3">
                     {/* Logo */}
@@ -18,7 +20,7 @@ const Navbar = () => {
                     </h1>
 
                     {/* Desktop Menu */}
-                    <ul className="hidden md:flex space-x-8 text-white font-medium">
+                    <ul className="hidden md:flex md:items-center space-x-8 text-white font-medium">
                         <li className="hover:text-cyan-400 transition-colors cursor-pointer">
                             Home
                         </li>
@@ -31,16 +33,33 @@ const Navbar = () => {
                         <li className="hover:text-cyan-400 transition-colors cursor-pointer">
                             Contact
                         </li>
-                       <Link href={'/blog'}>
-                          <li className="hover:text-cyan-400 transition-colors cursor-pointer">
-                            Blog
-                        </li>
-                       </Link>
-                       <Link href={'/login'}>
-                          <li className="hover:text-cyan-400 transition-colors cursor-pointer">
-                            Login
-                        </li>
-                       </Link>
+                        {
+                            session.status === "authenticated" &&
+                            <>
+                                <Link href={'/blog'}>
+                                    <li className="hover:text-cyan-400 transition-colors cursor-pointer">
+                                        Blog
+                                    </li>
+                                </Link>
+                                <Link href={'/dashboard'}>
+                                    <li className="hover:text-cyan-400 transition-colors cursor-pointer">
+                                        Dashboard
+                                    </li>
+                                </Link>
+                            </>
+                        }
+                        {
+                            session.status === "authenticated" ? 
+                            <button onClick={()=>signOut()} className="bg-sky-400 hover:bg-cyan-500 text-white rounded-2xl px-3 py-1 transition-colors cursor-pointer">
+                                Logout
+                            </button> :
+                                <Link href={'/login'}>
+                                    <li className="hover:text-cyan-400 transition-colors cursor-pointer">
+                                        Login
+                                    </li>
+                                </Link>
+                        }
+
                     </ul>
 
                     {/* Mobile Button */}
